2ad654420f7e60a685c0553062995b727bccb91b
admin <odyssey.apps.ltd.2@gmail.com>
2018-03-05 12:47:12 +0600

    Main Activity Added

--- app/src/main/java/com/odyssey/apps/collagingpic/MainActivity.java
+++ app/src/main/java/com/odyssey/apps/collagingpic/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         System.out.println();
         //This is Master
+        //
         setContentView(R.layout.activity_main);
     }
 }
